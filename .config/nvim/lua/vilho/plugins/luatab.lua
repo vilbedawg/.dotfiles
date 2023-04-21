e4fe33193e652070bec1209bb62fb0c190a0f71a
vilbedawg <vilho.luoma@gmail.com>
2023-04-21 18:36:15 +0300

    Ensure luatab is loaded

--- .config/nvim/lua/vilho/plugins/luatab.lua
+++ .config/nvim/lua/vilho/plugins/luatab.lua
@@ -1,6 +1,7 @@
 return {
 	"alvarosevilla95/luatab.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+  priority = 1000,
 	config = function()
 		require("luatab").setup({
 			separator = function()
