9647741f5caf320139662fa5bdf00c34be66a932
vilbedawg <vilho.luoma@gmail.com>
2023-10-10 14:22:37 +0300

    Use indent-blankline v3

--- .config/nvim/lua/vilho/plugins/indent-blankline.lua
+++ .config/nvim/lua/vilho/plugins/indent-blankline.lua
@@ -1,14 +1,11 @@
 vim.opt.list = true
 
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPost", "BufNewFile" },
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPost", "BufNewFile" },
   main = "ibl",
-	opts = {
-		char = " ",
-		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = false,
-	},
+  opts = {},
+  config = function()
+    require("ibl").setup()
+  end,
 }
