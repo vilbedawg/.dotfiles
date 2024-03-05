f9913fc19e16843a535fe1ca51b3eea5852e0ecc
vilbedawg <vilho.luoma@gmail.com>
2024-03-05 12:36:40 +0200

    Replace old null-ls with other fork

--- .config/nvim/lua/vilho/plugins/init.lua
+++ .config/nvim/lua/vilho/plugins/init.lua
@@ -1,17 +1,17 @@
 return {
   -- LSP stuff
-  { "neovim/nvim-lspconfig" },            -- Configs for the nvim lsp client
-  { "williamboman/mason.nvim" },          -- LSP server manager
+  { "neovim/nvim-lspconfig" },             -- Configs for the nvim lsp client
+  { "williamboman/mason.nvim" },           -- LSP server manager
   { "williamboman/mason-lspconfig.nvim" }, -- Helper for mason
-  { "onsails/lspkind-nvim" },             -- VScode-like pictograms
-  { "nvim-lua/plenary.nvim" },            -- Useful lua functions used by lots of plugins
-  { "lukas-reineke/lsp-format.nvim" },    -- A wrapper around Neovims native LSP formatting
-  { "jose-elias-alvarez/null-ls.nvim" },  -- For formatters and linters
-  { "jayp0521/mason-null-ls.nvim" },      -- For managing formatters and linters
+  { "onsails/lspkind-nvim" },              -- VScode-like pictograms
+  { "nvim-lua/plenary.nvim" },             -- Useful lua functions used by lots of plugins
+  { "lukas-reineke/lsp-format.nvim" },     -- A wrapper around Neovims native LSP formatting
+  { "nvimtools/none-ls.nvim" },            -- For formatters and linters
+  { "jayp0521/mason-null-ls.nvim" },       -- For managing formatters and linters
 
   -- Snippets
   {
-    "L3MON4D3/LuaSnip",            -- Snippet engine
+    "L3MON4D3/LuaSnip",               -- Snippet engine
     dependencies = {
       "rafamadriz/friendly-snippets", -- A bunch of useful snippets
       config = function()
@@ -21,23 +21,25 @@ return {
   },
 
   {
-    "hrsh7th/nvim-cmp",                   -- A completion engine
+    "hrsh7th/nvim-cmp",                      -- A completion engine
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",             -- cmp-source for neovim's built-in language client
-      "hrsh7th/cmp-buffer",               -- cmp-source for buffer words
-      "hrsh7th/cmp-path",                 -- cmp-source for filesystem paths
-      "saadparwaiz1/cmp_luasnip",         --Luasnip source for nvim-cmp
+      "hrsh7th/cmp-nvim-lsp",                -- cmp-source for neovim's built-in language client
+      "hrsh7th/cmp-buffer",                  -- cmp-source for buffer words
+      "hrsh7th/cmp-path",                    -- cmp-source for filesystem paths
+      "saadparwaiz1/cmp_luasnip",            --Luasnip source for nvim-cmp
       "hrsh7th/cmp-nvim-lsp-signature-help", -- Display functions signatures
-      "onsails/lspkind.nvim",             -- Icons for autocompletion
+      "onsails/lspkind.nvim",                -- Icons for autocompletion
     },
   },
 
-  { "folke/neodev.nvim" },    -- Neovim setup for init.lua and plugin development
-  { "folke/lsp-colors.nvim" }, -- LSP colors
+  { "folke/neodev.nvim",    opts = {} }, -- Neovim setup for init.lua and plugin development
+  { "folke/lsp-colors.nvim" },           -- LSP colors
 
   { "tpope/vim-repeat" },
   { "tpope/vim-surround" },
-  { "tpope/vim-unimpaired" },
+  {
+    "tpope/vim-unimpaired"
+  },
   { "tpope/vim-rhubarb" },
   {
     "tpope/vim-fugitive",
