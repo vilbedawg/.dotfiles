9363af00cffba3ee3dd99c0a77dc8be3f147e7ca
vilho luoma <vilho.luoma@futurice.com>
2024-09-07 22:27:39 +0300

    Update which-key to work with new v3 version

--- .config/nvim/lua/vilho/plugins/whichkey.lua
+++ .config/nvim/lua/vilho/plugins/whichkey.lua
@@ -1,26 +1,46 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		plugins = { spelling = true },
-	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register({
-			mode = { "n", "v" },
-			["]"] = { name = "+next" },
-			["["] = { name = "+prev" },
-			["<leader>t"] = { name = "+tabs" },
-			["<leader>c"] = { name = "+code" },
-			["<leader>f"] = { name = "+file/find" },
-			["<leader>g"] = { name = "+git" },
-			["<leader>q"] = { name = "+quit/session" },
-			["<leader>x"] = { name = "+diagnostics" },
-			["<leader>s"] = { name = "+search" },
-			["<leader>w"] = { name = "+windows" },
-			["<leader>b"] = { name = "+buffers" },
-			["g"] = { name = "+goto" },
-		})
-	end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    defaults = {},
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader>t", group = "tabs" },
+        { "<leader>w", group = "windows" },
+        { "<leader>b", group = "buffers" },
+        { "<leader>c", group = "code" },
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>h", group = "git hunks" },
+        { "<leader>s", group = "search" },
+        { "<leader>x", group = "diagnostics/quickfix" },
+        { "<leader>nh", group = "clear highlights" },
+        { "<leader>q",  group = "session" },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        { "g", group = "goto" },
+        { "gs", group = "surround" },
+        -- { "z", group = "fold" },
+      }
+    },
+    preset = "modern",
+    icons = {
+      mappings = false
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+  end,
 }
