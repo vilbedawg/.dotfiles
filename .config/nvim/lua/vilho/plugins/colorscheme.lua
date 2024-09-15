b0b974b4d8a5fdfd044f595f7f87c9989843a159
vilho luoma <vilho.luoma@futurice.com>
2024-09-15 10:25:12 +0300

    Change alacritty config and colorscheme

--- .config/nvim/lua/vilho/plugins/colorscheme.lua
+++ .config/nvim/lua/vilho/plugins/colorscheme.lua
@@ -1,7 +1,15 @@
 return {
-  'Mofiqul/vscode.nvim',
-  config = function()
-    vim.o.background = 'dark'
-    vim.cmd.colorscheme 'vscode'
-  end
+  "Mofiqul/vscode.nvim",
+  opts = {
+    italic_comments = true,
+    transparent = true,
+    disable_nvimtree_bg = true,
+    group_overrides = {
+      NormalFloat = { bg = "NONE" }, -- transparent background
+    },
+  },
+  init = function()
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("vscode")
+  end,
 }
