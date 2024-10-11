085b9af7a4a6bba627d5211e12b4a4e949dada61
vilho luoma <vilho.luoma@futurice.com>
2024-10-11 21:38:44 +0300

    update coloscheme to gruvbox

--- .config/nvim/lua/vilho/plugins/colorscheme.lua
+++ .config/nvim/lua/vilho/plugins/colorscheme.lua
@@ -1,15 +1,37 @@
 return {
-  "Mofiqul/vscode.nvim",
-  opts = {
-    italic_comments = true,
-    transparent = true,
-    disable_nvimtree_bg = true,
-    group_overrides = {
-      NormalFloat = { bg = "NONE" }, -- transparent background
-    },
-  },
-  init = function()
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("vscode")
+  -- Colorscheme
+  "ellisonleao/gruvbox.nvim",
+  config = function()
+    require("gruvbox").setup({
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = false,
+        operators = false,
+        comments = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {
+        SignColumn = { bg = "NONE" },
+      },
+      dim_inactive = false,
+      transparent_mode = false,
+    })
+    vim.cmd("colorscheme gruvbox")
+    vim.cmd("hi! GruvboxYellowSign guibg=NONE ")
+    vim.cmd("hi! GruvboxPurpleSign guibg=NONE ")
+    vim.cmd("hi! GruvboxOrangeSign guibg=NONE ")
+    vim.cmd("hi! GruvboxGreenSign guibg=NONE ")
+    vim.cmd("hi! GruvboxBlueSign guibg=NONE ")
+    vim.cmd("hi! GruvboxAquaSign guibg=NONE ")
+    vim.cmd("hi! GruvboxRedSign guibg=NONE ")
   end,
 }
