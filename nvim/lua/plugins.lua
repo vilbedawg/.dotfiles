a3bf2d989de0c65e35c7ba6e87512608dbe6b95c
vilho.luoma <vilho.luoma@gmail.com>
2025-12-16 13:12:28 +0200

    Update treesitter branch

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
