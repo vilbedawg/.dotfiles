cb973abf6eea0d551b2c8fa173ef428baea927a2
vilho.luoma <vilho.luoma@gmail.com>
2026-04-03 08:46:43 +0300

    Add incremental selection keybind

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -226,3 +226,20 @@ require("gitsigns").setup({
         map("n", "<leader>hq", gitsigns.setqflist, { desc = "send to qf list" })
     end,
 })
+
+-- incremental selection treesitter/lsp
+vim.keymap.set({ "n", "x", "o" }, "<A-o>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_parent(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(vim.v.count1)
+	end
+end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+vim.keymap.set({ "n", "x", "o" }, "<A-i>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_child(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(-vim.v.count1)
+	end
+end, { desc = "Select child treesitter node or inner incremental lsp selections" })
